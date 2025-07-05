2ba8bf1a27a0eed5ca3a462c2978c73f7f462ced
Mayank Agrawal <mayankagrawal7067@gmail.com>
2025-07-05 20:47:16 +0530

    temprorary commit

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture
+
 def driver():
       service_obj= Service(r"C:\Users\HP\Documents\Software Testing\Automation tools\chromedriver-win64\chromedriver.exe")
       driver=webdriver.Chrome(service=service_obj)
